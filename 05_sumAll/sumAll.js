5827773b9fd6bbcca73fc678294969ce93b313ad
Lakshya <cartoonshow57@gmail.com>
2022-03-13 13:25:58 +0530

    Completed js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (start > end) {
+
+        start += end;
+        end = start - end;
+        start -= end;
+    }
+
+    if (typeof start != "number" || typeof end != "number" || start < 0 || end < 0) return "ERROR"
+    return (end - start + 1) % 2 == 0 ? (end - start + 1) / 2 * (start + end) : (start + end) / 2 * (end - start + 1);
 };
 
 // Do not edit below this line
